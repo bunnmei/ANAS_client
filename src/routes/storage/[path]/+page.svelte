7192b59497ea6183eb397d9ebf29255c0bcdb9c3
bunnmei <kombinat.web@gmail.com>
2025-12-21 16:52:02 +0900

    folderエンドポイントから取得

--- src/routes/storage/[path]/+page.svelte
+++ src/routes/storage/[path]/+page.svelte
@@ -3,6 +3,26 @@
   export let data: PageData; 
   const  { path } = data;
 
+  import { onMount } from 'svelte';
+
+  onMount(async () => {
+    console.log("Fetching folder data for path:", path);
+    const apipath = "http://192.168.1.104:8080";
+    const res = await fetch( 
+      `${apipath}/folder`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ path: path }),
+      }
+    );
+    // console.log(res);
+    const folderData = await res.json();
+    console.log(folderData);
+  });
+
 </script>
 
 <h1>Path: {path}</h1>
